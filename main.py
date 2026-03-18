29e8b4964752f0178816568892ec6bca3b0e81cc
Nicholas Trimble <nicholas@jasontrimble.com>
2026-03-18 16:22:25 -0400

    Issue #1: implement staff_lines(). Works on test image, needs broader testing (incl. with scanned images) and might refactor the Hough lines part out to give a parameterizable interface for the line angles we search for

--- main.py
+++ main.py
@@ -15,7 +15,13 @@ output: tuple representing the Y-coordinate of each line of the staff, in pixels
         can also return angle input image is rotated by (degrees)
 '''
 def staff_lines(image):
-    print("hi")
+    test_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
+    h, theta, d = ski.transform.hough_line(image, theta=test_angles)
+
+    accum, angles, dist = ski.transform.hough_line_peaks(h, theta, d, min_distance=5, num_peaks=5)
+    dist = dist.astype(np.int32) #max image height of 2^32-1
+    return -1*dist, np.rad2deg(np.mean(angles))
+    
 
 #for debugging
 def imshow(image, isGray=True):
@@ -28,6 +34,8 @@ def imshow(image, isGray=True):
 def main():
     filepath = "Img/blow_single.png" #todo: make commandline arg
     im = ski.io.imread(filepath)
+
+    #Preprocessing
     if im.ndim == 3:            #colour image
         if im.shape[2] == 4:    #RGBA image
             im = ski.color.rgba2rgb(im)
@@ -37,4 +45,7 @@ def main():
         im = ski.util.img_as_ubyte(im)
 
     im = ski.util.invert(im)    #Its preferable for features to be represented by 255
-    imshow(im)
+    #imshow(im)
+
+    #Staff line location
+    lines, rotation = staff_lines(im)
