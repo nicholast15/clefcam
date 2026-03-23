c79baa755010e87f31b52b596f150023a9e9cc16
Nicholas Trimble <nicholas@jasontrimble.com>
2026-03-23 15:27:12 -0400

    Complete image slicing, run clef detection on first slice (#5).

--- main.py
+++ main.py
@@ -48,6 +48,20 @@ def staff_borders(lines, dims):
     return borders
 
 '''
+Input: Image of sheet music we're processing, border array from staff_borders
+Output: Array of images representing a line of bars each
+'''
+def staff_slice(image, borders):
+    slices = []
+    i = 0
+    for j in range(0, len(borders)-1):
+        start = borders[j]
+        end = borders[j+1]-1    #dont include the start of the next slice
+        slices.append(image[start:end])
+        
+    return slices
+
+'''
 performs singular template matching, returning score and location of best match
 '''
 def template_match(image, template):
@@ -77,13 +91,14 @@ def clef_match(line):
             if sizes[i] > h:
                 break
             size = sizes[i]
-        print(size)
+        #print(size)
 
         for clef in clefs:  #check all clefs at the determined size
             template_file = template_root + clef + '_' + str(size) + ".png"
             template = cv.imread(template_file, cv.IMREAD_GRAYSCALE)
             assert template is not None, "Template file " + template_file + " not found"
             max_val, max_loc = template_match(line, template)
+            #print(max_val, max_loc)
             score.append(max_val)
 
         match_i = np.argmax(np.array(score))    #find the clef with the highest match value
@@ -113,19 +128,23 @@ def main(filepath):
         im = ski.util.img_as_ubyte(im)
 
     im = ski.util.invert(im)    #Its preferable for features to be represented by 255
-    imshow(im)
+    #imshow(im)
 
     #Staff line location
     lines, rotation = staff_lines(im)
-    print(lines, rotation)
-    staff_borders(lines, im.shape)
+    #print(lines, rotation)
 
     #rotate the image based on rotation
     #for now assume horizontal: below slicing will need to account for lines being horiz+vert comps
 
     #slice the image into stripes based on groups of 5 in lines
+    borders = staff_borders(lines, im.shape)
+    slices = staff_slice(im, borders)
+    #for s in slices:
+    #    imshow(s)
 
     #check the clef and signature
-
+    clef = clef_match(ski.util.invert(slices[0])) #just check for clef on the first line
+    print(clef)
 
 
