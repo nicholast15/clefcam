01a4873b31f0a84e872f4917e0e03c8186135a8d
Nicholas Trimble <nicholas@jasontrimble.com>
2026-04-02 18:56:59 -0400

    Make key templates smaller

---
+++ Img/templates/key_render.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SIZES=(8 16 32)
+
+for svg_file in sharp.svg; do
+	filename=$(basename "$svg_file" .svg)
+
+	for size in "${SIZES[@]}"; do
+		convert "$svg_file" -resize "${size}x${size}" \
+            	-quality 500 -strip \
+            	"${filename}_${size}.png"
+	done
+	echo "Processed $filename"
+done
